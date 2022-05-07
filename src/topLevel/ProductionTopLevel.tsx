fc8ddc26c8f333ccacb78455b3aa0fa4309d8a8e
Blake Davison <b.davison16@gmail.com>
2022-05-06 19:38:04 -0700

    Initial commit with some structures

---
+++ src/topLevel/ProductionTopLevel.tsx
@@ -0,0 +1,6 @@
+import { FC } from "react"
+import TopLevel from "./TopLevel"
+
+const ProductionTopLevel:FC = () => <TopLevel />
+
+export default ProductionTopLevel
---
+++ src/topLevel/TopLevel.tsx
@@ -0,0 +1,11 @@
+import { FC} from "react"
+
+const TopLevel:FC = ():JSX.Element => {
+    return(
+        <div>
+            taskName
+        </div>
+    )
+}
+
+export default TopLevel
