a6b7189a1d487afe4894bfb63d940c89a6af15dd
GenerNom <greenmage13@gmail.com>
2022-05-20 19:51:58 -0700

    clean up code

--- src/list/ListManager.tsx
+++ src/list/ListManager.tsx
@@ -1,16 +1,21 @@
 import { FC } from 'react'
 import Spell from '../Spell'
-import SpellFormatting from './SpellFormatting'
+
+const SpellFormatting: FC<{spell: Spell}> = ({spell}): JSX.Element => {
+    return (
+        <li>
+            {spell.name}
+        </li>
+    )
+}
 
 const ListManager: FC<{allSpells: Array<Spell>}> = ({allSpells}) => {
-    const DisplaySpells: FC<{list: Array<Spell>}> = ({list}): JSX.Element[] => {
-        return list.map((spell): JSX.Element => {
-            return <SpellFormatting spell={spell} />
-        })
-    }
+    
     return (
         <div id="ListManager">
-            <ul>{<DisplaySpells list={allSpells} />}</ul>
+            <ul>{allSpells.map((spell): JSX.Element => {
+                return <SpellFormatting spell={spell} />
+            })}</ul>
         </div>
     )
 }
