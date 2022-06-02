8c96dbd32b186389183fbbaf5160bdb22100330d
GenerNom <greenmage13@gmail.com>
2022-06-01 22:01:08 -0700

    remove unused log

--- src/topLevel/ProductionTopLevel.tsx
+++ src/topLevel/ProductionTopLevel.tsx
@@ -5,7 +5,6 @@ import getSpellList from "../spellListService"
 import { ListManager } from "../list/ListManager"
 
 const importedSpells:Array<Spell> = getSpellList()
-console.log(importedSpells.length);
 
 const ProductionListManager:FC = ():JSX.Element => <ListManager allSpells={importedSpells} /> 
 const ProductionTopLevel:FC = ():JSX.Element => <TopLevel ListManager={ProductionListManager} />
